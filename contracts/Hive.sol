6058bd75f48908818ef9e47118e819d196f6f49e
mattiapomelli <mattiapomelli@gmail.com>
2023-06-09 15:02:46 +0200

    Setup contracts

--- contracts/Hive.sol
+++ contracts/Hive.sol
@@ -1,4 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-contract Hive {}
+import {ITalentLayerID} from "./interfaces/ITalentLayerID.sol";
+
+/**
+ * @title Hive
+ * @dev This contract is used to manage a group of freelancers (hive). It allows for the creation
+ *      of new proposals, reviews, and payments.
+ */
+contract Hive {
+    ITalentLayerID talentLayerId;
+
+    mapping(uint256 => bool) members;
+
+    // =========================== Constructor ==============================
+
+    /**
+     * @param talentLayerIdAddress The address of the TalentLayerID contract.
+     */
+    constructor(address talentLayerIdAddress, string memory dataUri, address owner) {
+        talentLayerId = ITalentLayerID(talentLayerIdAddress);
+    }
+}
--- contracts/HiveFactory.sol
+++ contracts/HiveFactory.sol
@@ -1,4 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-contract HiveFactory {}
+import {Hive} from "./Hive.sol";
+import {ITalentLayerID} from "./interfaces/ITalentLayerID.sol";
+
+/**
+ * @title HiveFactory
+ * @dev This contract is used to create new Hive (group) contracts.
+ */
+contract HiveFactory {
+    ITalentLayerID talentLayerId;
+
+    // =========================== Constructor ==============================
+
+    /**
+     * @param talentLayerIdAddress The address of the TalentLayerID contract.
+     */
+    constructor(address talentLayerIdAddress) {
+        talentLayerId = ITalentLayerID(talentLayerIdAddress);
+    }
+
+    // =========================== User functions ==============================
+
+    /**
+     * Creates a new Hive (group) contract.
+     * @param dataUri The uri of the hive metadata.
+     */
+    function createHive(string memory dataUri) public {
+        // deploy new Hive contract
+        Hive hive = new Hive(address(talentLayerId), dataUri, msg.sender);
+    }
+}
